2dddba7611b63ee88ac7f73ae715fdba60a27c6e
Suraj Maurya <144997298+suraj3641@users.noreply.github.com>
2025-03-30 12:48:47 +0530

    first commit

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
-themeToggle.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  const isDarkMode = body.classList.contains('dark-mode');
-  themeToggle.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
-});
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
 
 
 
-document.getElementById('learnMore').addEventListener('click', function() {
-  alert('Thank you for your interest! More details coming soon.');
-});
+function toggleNavbar() {
+  document.getElementById("navbarNav").classList.toggle("active");
+}
+
+
 
 
 
